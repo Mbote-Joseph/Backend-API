c49520a26ff11d491969d57f6b354cb96e0bbbd8
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-07 16:41:00 +0300

    Project Backend

--- restApi/models.py
+++ restApi/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+    farmerName = models.CharField(max_length=100)
     name = models.CharField(max_length=200)
     price = models.FloatField()
     quantity = models.IntegerField()
     description = models.TextField()
+    location = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
-    
+
 
 
     def __str__(self):
