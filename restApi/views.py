3c8b020f7ee07d6f3a0998fe904d966b4e472e6c
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-07 13:27:09 +0300

    Project Backend

--- restApi/views.py
+++ restApi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def getItems(request):
+    return Response({'items': ['item1', 'item2']})
+
+
+@api_view(['POST'])
+def addItem(request):
+    return Response({'items': ['item1', 'item2']})
