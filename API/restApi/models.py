1fbf4667c3bd4e3ec853fdf39d158efe6bd81d26
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-11 22:15:08 +0300

    Project Backend

--- API/restApi/models.py
+++ API/restApi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
