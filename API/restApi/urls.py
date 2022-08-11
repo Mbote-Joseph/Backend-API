06d1dd08152ea73200a88aafff5240d9279ec3b7
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-11 22:34:41 +0300

    Project Backend

--- API/restApi/urls.py
+++ API/restApi/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
     path('get', views.getItems, name='getItems'),
-    path('post', views.postItems, name='postItems'),
+    path('post', views.postItem, name='postItem'),
     path('update/<int:pk>', views.updateItem, name='updateItem'),
     path('delete/<int:pk>', views.deleteItem, name='deleteItem'),
 ]
