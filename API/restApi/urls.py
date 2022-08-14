4f94d79af7d8f3c459ae6def19436ec4b42193eb
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-14 23:02:21 +0300

    Project Backend

--- API/restApi/urls.py
+++ API/restApi/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('get', views.getItems, name='getItems'),
-    path('post', views.postItem, name='postItem'),
-    path('update/<int:pk>', views.updateItem, name='updateItem'),
-    path('delete/<int:pk>', views.deleteItem, name='deleteItem'),
+    path('item/get', views.getItems, name='getItems'),
+    path('item/post', views.postItem, name='postItem'),
+    path('item/update/<int:pk>', views.updateItem, name='updateItem'),
+    path('item/delete/<int:pk>', views.deleteItem, name='deleteItem'),
+    path('book/get', views.getBooks, name='getBooks'),
+    path('book/post', views.postBook, name='postBook'),
+    path('book/update/<int:pk>', views.updateBook, name='updateBook'),
+    path('book/delete/<int:pk>', views.deleteBook, name='deleteBook'),
 ]
