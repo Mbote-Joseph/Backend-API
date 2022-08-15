0f765b42234cdfc09c3aefa5d03cb17a461620b6
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-15 18:56:20 +0300

    Project Backend

--- API/API/settings.py
+++ API/API/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
 
     # Apps
     'rest_framework',
-    'restApi'
+    'restApi',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
