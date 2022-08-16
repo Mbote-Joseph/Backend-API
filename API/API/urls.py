ddef2182d04e2efbddcb462010594bcd78ebb311
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-16 11:42:34 +0300

    API docuumentation

--- API/API/urls.py
+++ API/API/urls.py
@@ -32,7 +32,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    authentication_classes={}
+    authentication_classes=[]
 )
 
 
@@ -43,7 +43,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('', include('restApi.urls')),
-    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
